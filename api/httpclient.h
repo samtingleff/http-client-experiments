52205d3b507482f15729492f69f8344975cc42de
Sam Tingleff <sam@tingleff.com>
2013-04-19 16:27:20 -0700

    C header file with draft API

---
+++ api/httpclient.h
@@ -0,0 +1,86 @@
+#include <stddef.h>
+
+#define RUBICON_HTTP_METHOD_GET 1
+#define RUBICON_HTTP_METHOD_POST 2
+
+#define ERRORS_DNS 1
+#define ERRORS_CONNECTION_CLOSED = 2
+#define ERRORS_TIMEOUT 3
+// there should be more of these
+
+typedef struct
+{
+	size_t len;
+	unsigned char data[];
+} bytestring;
+
+typedef struct
+{
+	// name ("Content-Type")
+	bytestring name;
+
+	// value ("application/json")
+	bytestring value;
+} http_header;
+
+typedef struct
+{
+	// unique identifier for this request object
+	// provided back to the caller in http_client_response
+	int request_id;
+
+	// destination URL
+	bytestring url;
+
+	// HTTP method (GET/POST; see above defines)
+	int method;
+
+	// request (POST) body, if there is one
+	bytestring request_body;
+
+	// request headers, if any
+	http_header headers[];
+} http_client_request;
+
+typedef struct
+{
+	// overall millisecond timeout to return to the caller
+	int timeout;
+
+	// list of requests
+	http_client_request requests[];
+} http_client_multirequest;
+
+typedef struct
+{
+	// unique identifier from http_client_request
+	int request_id;
+
+	// all possible statuses that do not result in a valid http response
+	// from the destination URL.
+	// see ERRORS_DNS, etc defined above
+	int request_status;
+
+	// http status code returned
+	int http_response_code;
+
+	// response body
+	bytestring request_body;
+
+	// response headers
+	http_header headers[];
+} http_client_response;
+
+typedef struct
+{
+	// total time in millis spent on this request
+	int response_time;
+
+	// all responses
+	http_client_response responses[];
+} http_client_multiresponse;
+
+/**
+ * Submit requests, receive responses.
+ */
+http_client_multiresponse request(http_client_multirequest requests);
