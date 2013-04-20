9d6e50b12197ef97d7c74a8f7804c645e9f04fa5
Sam Tingleff <sam@tingleff.com>
2013-04-19 22:08:02 -0700

    sleep for a random time period

--- simulation/bidder.js
+++ simulation/bidder.js
@@ -2,18 +2,31 @@
 var util = require('util'),
     http = require('http');
 
-http.createServer(function (req, res) {
-  // modify sleepTime to increase the fun
-  var sleepTime = 60;
+var createServer = function(port) {
+  http.createServer(function(req, res) {
+    // sleep for a random time between 40 and 150 ms
+    var sleepTime = (Math.random() * (150 - 40)) + 40;
 
-  var response = {
-    status : 10,
-    hello : "world"
-  };
-  var responseBody = JSON.stringify(response);
-  setTimeout(function() {
-    res.writeHead(200, {'Content-Type':'application/json', 'Content-Length':responseBody.length});
-    res.write(responseBody);
-    res.end();
-  }, sleepTime);
-}).listen(8124);
+    var response = {
+      status : 10,
+      hello : "world"
+    };
+    var responseBody = JSON.stringify(response);
+    setTimeout(function() {
+      res.writeHead(200, {'Content-Type':'application/json', 'Content-Length':responseBody.length});
+      res.write(responseBody);
+      res.end();
+    }, sleepTime);
+  }).listen(port);
+};
+
+createServer(8124);
+createServer(8125);
+createServer(8126);
+createServer(8127);
+createServer(8128);
+createServer(8129);
+createServer(8130);
+createServer(8131);
+createServer(8132);
+createServer(8133);
